fa86fdd92cde2a8e8d12ee498a686aef8af04da0
Jonah <wander.shadow.cat@gmail.com>
2021-01-17 15:40:32 -0500

    updated Mapper classes, pull request is needed for next implementatoin step

--- src/main/java/rushb/webapp/mapper/ArticleMapper.java
+++ src/main/java/rushb/webapp/mapper/ArticleMapper.java
@@ -1,7 +1,7 @@
 package rushb.webapp.mapper;
 
 
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
 import rushb.webapp.model.Article;
 
 import java.util.List;
@@ -9,22 +9,36 @@ import java.util.List;
 @Mapper
 public interface ArticleMapper {
 
+    @Select("select * from article")
     List<Article> list();
 
+    @Select("select * from article where author = #{name}")
     List<Article> listByAuthorName(String name);
 
+    @Select("")
     List<Article> listByAuthorId(long AuthorId);
 
+    @Select("")
     List<Article> listByTag(String tag);
 
+    @Select("select * from article where id = #{id}")
     Article findById(long id);
 
+    @Select("select * from article where name = #{name}")
     Article findByName(String name);
 
+    @Delete("delete from article where id = #{id}")
     void deleteArticle(long id);
 
+    //TODO
+    @Update("update article set ...")
     void updateArticle(long id);
 
+    @Insert("insert into article(article_id, name, create_date, modify_date, hashtag, status, author) values" +
+            "(#{id}, #{name}, #{createDate}, #{modifyDate}, #{hashtag}, #{status}, #{author}")
+//    void save(@Param("id") Long id, @Param("name") String name, @Param("createDate")Date createDate,
+//              @Param("modifyDate") Date modifyDate, @Param("hashtag") List<String> hashtag,
+//              @Param("status") String status, @Param("author") String author);
     void save(Article article);
 
 }
--- src/main/java/rushb/webapp/mapper/UserMapper.java
+++ src/main/java/rushb/webapp/mapper/UserMapper.java
@@ -1,6 +1,8 @@
 package rushb.webapp.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import rushb.webapp.model.Article;
 import rushb.webapp.model.User;
 
@@ -9,12 +11,16 @@ import java.util.List;
 @Mapper
 public interface UserMapper {
 
+    @Select("select articleList from user where id = #{id}")
     List<Article> getAllArticle();
 
+    @Update("update user set ...")
     void updateUser(long id);
 
+    @Select("select * from user where id = #{id}")
     User findById(long id);
 
+    @Select("select * from user where name = #{name}")
     User findByName(String name);
 
 
